3dd73b2cecd8d5d95d47a845f8161c002ef0dffd
Jamilon <jamilon.mohammad@gmail.com>
2026-03-13 09:57:33 +0800

    Initial portfolio website

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+</head>
+
+<body>
+
+<h1>Hello! My Portfolio is Working.</h1>
+<p>This website is built with PHP.</p>
+
+</body>
+</html>
